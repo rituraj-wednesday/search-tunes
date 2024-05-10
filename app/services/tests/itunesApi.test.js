2906bbddeb81459c049ffca8fe9a7d3ee23a81dc
Rituraj Ingale <rituraj@wednesday.is>
2024-05-10 13:05:28 +0530

    Feat: testcase for iTunes API fixed with adding client type

--- app/services/tests/itunesApi.test.js
+++ app/services/tests/itunesApi.test.js
@@ -1,20 +1,20 @@
 import MockAdapter from 'axios-mock-adapter';
-import { getApiClient } from '@utils/apiUtils';
+import { API_TYPES, getApiClient } from '@utils/apiUtils';
 import { getSearchedList } from '../itunesApi';
 
 describe('ITunes API tests', () => {
   const term = 'jack+johnson';
   it('should make the api call to "/search?term="', async () => {
-    // const mock = new MockAdapter(getApiClient().axiosInstance);
-    // const data = [
-    //   {
-    //     totalCount: 1,
-    //     items: [{ repositoryName }]
-    //   }
-    // ];
-    // mock.onGet(`/search?term=${term}`).reply(200, data);
-    // const res = await getSearchedList(term);
-    // console.log(res);
-    // expect(res.data).toEqual(data);
+    const mock = new MockAdapter(getApiClient(API_TYPES.ITUNES).axiosInstance);
+    const data = {
+      resultCount: 1,
+      results: [{
+        trackId: 1,
+        trackName: 'Song'
+      }]
+    };
+    mock.onGet(`/search?term=${term}`).reply(200, data);
+    const res = await getSearchedList(term);
+    expect(res.data).toEqual(data);
   });
 });
