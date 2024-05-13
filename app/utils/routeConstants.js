6e286848fddd73fc2609e1dd6bd4b9f81745ca57
rituraj-wednesday <rituraj@wednesday.is>
2024-05-13 17:59:22 +0530

    Revert "Feature: Search List Container: Search Input , Reducers and Selectors added"

--- app/utils/routeConstants.js
+++ app/utils/routeConstants.js
@@ -6,13 +6,5 @@ export default {
       padding: 20
     },
     exact: true
-  },
-  searchTunes: {
-    route: '/searchTunes',
-    props: {
-      maxwidth: 500,
-      padding: 20
-    },
-    exact: true
   }
 };
