7fa511488c81476abbda63911e4bac89e86bab5a
Rituraj Ingale <rituraj@wednesday.is>
2024-05-13 16:10:02 +0530

    Feat: API type string added from enum

--- app/services/itunesApi.js
+++ app/services/itunesApi.js
@@ -1,4 +1,4 @@
-import { generateApiClient } from '@utils/apiUtils';
-const itunesApi = generateApiClient('itunes');
+import { API_TYPES, generateApiClient } from '@utils/apiUtils';
+const itunesApi = generateApiClient(API_TYPES.ITUNES);
 
 export const getSearchedList = (keyword) => itunesApi.get(`/search?term=${keyword}`);
