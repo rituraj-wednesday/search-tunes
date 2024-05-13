87bbc792df7e5833a01b6f16271c1d759cfb5af8
Rituraj Ingale <rituraj@wednesday.is>
2024-05-13 18:05:21 +0530

    Feat: Audio Controller created for audio state handling

--- app/utils/audioController.js
+++ app/utils/audioController.js
@@ -2,20 +2,45 @@
 /* eslint-disable immutable/no-mutation */
 class AudioController {
   constructor() {
-    this.audioTracks = {};
-    this.currentTrackID = null;
-    this.audioCtx = new AudioContext();
+    this.audioData = {}; // Object to store audio data
+    this.currentlyPlaying = null; // Keep track of currently playing audio
   }
 
-  static registerAudio(trackData) {
-    const { trackId, trackURL, ...rest } = trackData;
-    this.audioTracks.push({
-      trackId,
-      trackURL,
-      metadata: {
-        ...rest
-      }
-    });
+  // Function to register audio
+  registerAudio(trackId, audioUrl, otherData) {
+    const audio = new Audio(audioUrl);
+    this.audioData[trackId] = {
+      audio,
+      otherData
+    };
+    audio.preload = 'auto'; // Preload the audio
+  }
+
+  // Function to play audio by trackId
+  play(trackId) {
+    // If there is an audio currently playing, stop it
+    if (this.currentlyPlaying !== null) {
+      this.stop();
+    }
+
+    // Play the audio corresponding to the trackId
+    const audioObj = this.audioData[trackId];
+    if (audioObj) {
+      audioObj.audio.play();
+      this.currentlyPlaying = trackId;
+    } else {
+      console.error('Audio not found for trackId:', trackId);
+    }
+  }
+
+  // Function to stop currently playing audio
+  stop() {
+    if (this.currentlyPlaying !== null) {
+      const audioObj = this.audioData[this.currentlyPlaying];
+      audioObj.audio.pause();
+      audioObj.audio.currentTime = 0;
+      this.currentlyPlaying = null;
+    }
   }
 }
 
