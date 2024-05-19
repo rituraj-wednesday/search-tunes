ff449b9a414eb08880ead6971a87a9e60bf81732
Rituraj Ingale <rituraj@wednesday.is>
2024-05-19 13:12:54 +0530

    Feat: TrackReduxProvider reducer test coverage updated

--- app/containers/TrackReduxProvider/tests/reducer.test.js
+++ app/containers/TrackReduxProvider/tests/reducer.test.js
@@ -1,12 +1,12 @@
 import { trackReduxTypes, trackReduxProviderReducer, initialState } from '../reducer';
 
 describe('Track Redux Provider tests', () => {
+  it('should return the initial state by default', () => {
+    const res = trackReduxProviderReducer(undefined, {})
+    expect(res).toEqual(initialState);
+  });
+
   describe('Search List reducer tests', () => {
-    it('should return the initial state by default', () => {
-      const res = trackReduxProviderReducer(undefined, {})
-      expect(res).toEqual(initialState);
-    });
-  
     it('should return the updated state when an action of type REQUEST_GET_SEARCHED_TUNES is dispatched', () => {
       const dummyTerm = 'Jack Johnsonn';
       const expectedResult = {
@@ -70,6 +70,69 @@ describe('Track Redux Provider tests', () => {
   });
 
   describe('TrackInfo reducer tests', () => {
-
+    const tempTrackID = 1;
+    const tempTrack = [{
+      trackId: tempTrackID,
+    }]
+    it('should return the updated state when an action of type REQUEST_GET_TRACK_INFO is dispatched', () => {const dummyTerm = 'Jack Johnsonn';
+      const expectedResult = {
+        ...initialState,
+        trackId: tempTrackID,
+        loading: true,
+      };
+      expect(
+        trackReduxProviderReducer(initialState, {
+          type: trackReduxTypes.REQUEST_GET_TRACK_INFO,
+          trackId: tempTrackID
+        })
+      ).toEqual(expectedResult);
+    });
+    it('should return the updated state when an action of type SUCCESS_GET_TRACK_INFO is dispatched', () => {const dummyTerm = 'Jack Johnsonn';
+      const expectedResult = {
+        ...initialState,
+        trackInfo: tempTrack,
+        error: null,
+        loading: false,
+      };
+      expect(
+        trackReduxProviderReducer(initialState, {
+          type: trackReduxTypes.SUCCESS_GET_TRACK_INFO,
+          data: tempTrack,
+        })
+      ).toEqual(expectedResult);
+    });
+    it('should return the updated state when an action of type FAILURE_GET_TRACK_INFO is dispatched', () => {const dummyTerm = 'Jack Johnsonn';
+      const message = 'Something went wrong.';;
+      const expectedResult = {
+        ...initialState,
+        trackInfo: null,
+        error: message,
+      };
+      expect(
+        trackReduxProviderReducer(initialState, {
+          type: trackReduxTypes.FAILURE_GET_TRACK_INFO,
+          error: {
+            message,
+          },
+        })
+      ).toEqual(expectedResult);
+    });
+    it('should return the updated state when an action of type SET_TRACK_INFO_FROM_STATE is dispatched', () => {const dummyTerm = 'Jack Johnsonn';
+      const expectedResult = {
+        ...initialState,
+        trackInfo: {
+          resultCount: tempTrack.length,
+          results: tempTrack,
+        },
+        trackId: tempTrackID,
+        loading: false,
+      };
+      expect(
+        trackReduxProviderReducer(initialState, {
+          type: trackReduxTypes.SET_TRACK_INFO_FROM_STATE,
+          results: tempTrack,
+        })
+      ).toEqual(expectedResult);
+    });
   });
 });
